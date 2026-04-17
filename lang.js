1ce2aca28f35756ca335c5297d0c6af21339daad
N1kolayKot <160266693+NikolayKot02@users.noreply.github.com>
2026-04-17 15:08:01 +0200

    Add comments to translations in lang.js
    Added comments for clarity in Russian and English translations.

--- lang.js
+++ lang.js
@@ -1,20 +1,44 @@
 const translations = {
     ru: {
+        // Главная
         titlePart1: "TCK",
         titlePart2: "Team",
         menuSostav: "Составы",
         menuMatches: "Матчи",
         menuSocial: "Соцсети",
+        
+        // Страница состава
+        sostavTitle: "Состав команды",
+        cap: "(Капитан)",
         backBtn: "← Назад на главную",
-        sostavTitle: "Состав команды"
+
+        // Страница соцсетей
+        followTitle: "ПОДПИСЫВАЙТЕСЬ",
+        platformLabel: "Платформа",
+        instaLabel: "Инстаграм",
+        ytLabel: "Ютуб",
+        dsLabel: "Дискорд",
+        ttLabel: "ТикТок"
     },
     en: {
+        // Main
         titlePart1: "TCK",
         titlePart2: "Team",
         menuSostav: "Rosters",
         menuMatches: "Matches",
         menuSocial: "Social",
+
+        // Sostav Page
+        sostavTitle: "Team Roster",
+        cap: "(Captain)",
         backBtn: "← Back to Main",
-        sostavTitle: "Team Roster"
+
+        // Social Page
+        followTitle: "FOLLOW US",
+        platformLabel: "Platform",
+        instaLabel: "Instagram",
+        ytLabel: "YouTube",
+        dsLabel: "Discord",
+        ttLabel: "TikTok"
     }
 };
